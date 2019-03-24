a49a91ad08d739e426eee1e23e3aa77d48ff367b
Chillen <josh.jenkin@live.com>
2019-03-24 13:50:05 -0600

    new pics

--- src/components/Pics.js
+++ src/components/Pics.js
@@ -13,7 +13,7 @@ function Pictures(props) {
         <div>
                 {/* {props.results.map(result => ( */}
                 {/* key={result.id} */}
-                    <div className="thumbnail float-left mr-5 mb-5 shadow border border-dark" key={props.id} onClick={() => props.clickImage(props.id)} >
+                    <div className="thumbnail float-left mr-4 mb-4 shadow-lg p-3 rounded border border-info" key={props.id} onClick={() => props.clickImage(props.id)} >
                         <img style={{ width: "200px", height: "200px", borderWidth: "10px" }} alt="A Pic" src={props.image} />
                     </div>
                     
