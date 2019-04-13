03c066ff579968a49a4a00473c2b3527088b0c74
Chillen <josh.jenkin@live.com>
2019-04-13 15:44:56 -0600

    fixed pics

--- src/components/Pics.js
+++ src/components/Pics.js
@@ -11,13 +11,9 @@ function Pictures(props) {
     return (
 
         <div>
-                {/* {props.results.map(result => ( */}
-                {/* key={result.id} */}
-                    <div className="thumbnail float-left mr-4 mb-4 shadow-lg p-3 rounded border border-info" key={props.id} onClick={() => props.clickImage(props.id)} >
-                        <img style={{ width: "200px", height: "200px", borderWidth: "10px" }} alt="A Pic" src={props.image} />
-                    </div>
-                    
-                {/* ))} */}
+            <div className="thumbnail float-left mr-4 mb-4 shadow-lg p-3 rounded border border-info" key={props.id} onClick={() => props.clickImage(props.id)} >
+                <img style={{ width: "200px", height: "200px", borderWidth: "10px" }} alt="A Pic" src={props.image} />
+            </div>
         </div>
 
     );
